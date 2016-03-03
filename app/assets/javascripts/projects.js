dd2325ff1a4825c5d590b0da5b00b774b095a0ce
Adrien Couture <coutureadrien@gmail.com>
2016-03-03 16:15:51 -0500

    Link jQuery, create gEBI JS function, and highlight button

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,7 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).on('ready page:load', function( {
+$(document).on('ready page:load', function() {
+
   $('#search-form').on('submit', function(event){
     event.preventDefault();
 
@@ -13,4 +14,32 @@ $(document).on('ready page:load', function( {
       dataType: 'script'
     });
   });
+
+  function gEBI(id) {
+    return document.getElementById(id);
+  }
+
+  var italicYellowBgApplier
+
+  function toggleItalicYellowBg() {
+    italicYellowBgApplier.toggleSelection();
+  }
+
+  window.onload = function() {
+    rangy.init();
+
+    var classApplierModule = rangy.modules.ClassApplier || rangy.modules.CssClassApplier;
+
+    italicYellowBgApplier = rangy.createCssClassApplier('italicYellowBg', {
+      tagNames: ['span', 'a', 'b']
+    });
+
+    var toggleItalicYellowBgButton = gEBI('toggleItalicYellowBgButton');
+    toggleItalicYellowBgButton.disabled = false;
+    toggleItalicYellowBgButton.ontouchstart = toggleItalicYellowBgButton.onmousedown = function() {
+      toggleItalicYellowBg();
+      return false;
+    };
+  }
+
 });
