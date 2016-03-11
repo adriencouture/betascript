eadd983319df55f2e31a175b578e5de824597b20
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-03-10 23:42:48 -0500

    Enter notes and preliminary tag method in Controller to sanitize and separate tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,14 @@
 class Tag < ActiveRecord::Base
-  has_and_belongs_to_many :projects 
+  has_and_belongs_to_many :projects
+
+  # Hypothesis logic for how a string of words "travel france wine food" needs
+  # to be manipulated in order to be saved to the database as:
+  # "travel", "france", "wine", "food" in the tags model, each with its own ID.
+  
+  def separate_tags(tag_list)
+    @tag_array = tag_list.split(" ")
+    @tag_array.each do |tag|
+      return tag.capitalize
+    end
+  end
 end
