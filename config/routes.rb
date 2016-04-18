d4fd14e744fb61f34fc4ccbf75dd31dbfcfbe70d
Adrien Couture <coutureadrien@gmail.com>
2016-04-18 11:54:44 -0400

    Mobile menu update hover colour

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
 
+  # Why doesn't the following work?
+  get 'login', to: 'user_sessions#new', as: :login
+  post 'logout', to: 'user_sessions#destroy', as: :logout
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
